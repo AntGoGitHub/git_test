787227ed0e3d7e3170f34fc12ff14b85fb9707a1
ccl <ccl188@cc.com>
2022-07-24 10:49:24 +0800

    git_test3

--- src/main/java/com/ccl/git/GitTest.java
+++ src/main/java/com/ccl/git/GitTest.java
@@ -3,7 +3,8 @@ package com.ccl.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hellp git!");
-        System.out.println("hellp github!");
+        System.out.println("hellp github!2");
+        System.out.println("hellp github!3");
     }
 }
 
