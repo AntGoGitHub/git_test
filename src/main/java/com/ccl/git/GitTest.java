f99dda20245e1d1da4d8d2bcb2bbb70edad41140
ccl <ccl188@cc.com>
2022-07-24 11:01:24 +0800

    git_test4 hot-fix commit1

--- src/main/java/com/ccl/git/GitTest.java
+++ src/main/java/com/ccl/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hellp github!2");
         System.out.println("hellp github!3");
         System.out.println("hellp github!4 hot-fix!");
+        System.out.println(" hot-fix!2");
     }
 }
 
